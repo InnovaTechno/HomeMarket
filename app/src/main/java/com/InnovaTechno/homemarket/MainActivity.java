9e47aba2ef9244b13f914fb8b4dc344910262185
ElizabethDum12 <elizabethdumervil@gmail.com>
2019-09-10 16:52:47 -0400

    Bottom navigation added

--- app/src/main/java/com/InnovaTechno/homemarket/MainActivity.java
+++ app/src/main/java/com/InnovaTechno/homemarket/MainActivity.java
@@ -1,14 +1,43 @@
 package com.InnovaTechno.homemarket;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_home:
+                        Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_favorites:
+                        break;
+                    case R.id.action_cart:
+                        break;
+                    case R.id.action_profile:
+                    default:
+                        break;
+                }
+                return true;
+            }
+        });
+
     }
 }
