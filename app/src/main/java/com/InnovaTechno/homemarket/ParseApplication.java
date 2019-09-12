22a37938a6f041a1ee564c01b3e6652645a7d3f3
Rocket91 <jeancharlesyvenson@hotmail.com>
2019-09-11 20:42:46 -0400

    parseAppplication created post model created

---
+++ app/src/main/java/com/InnovaTechno/homemarket/ParseApplication.java
@@ -0,0 +1,23 @@
+package com.InnovaTechno.homemarket;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+public class ParseApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        ParseObject.registerSubclass(Post.class);
+        // set applicationId, and server server based on the values in the Heroku settings.
+        // clientKey is not needed unless explicitly configured
+        // any network interceptors must be added with the Configuration Builder given this syntax
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("homemarket") // should correspond to APP_ID env variable
+                .clientKey("homemarket-app")  // set explicitly unless clientKey is explicitly configured on Parse server
+                .server("http://homemarket.herokuapp.com/parse").build());
+    }
+
+}
