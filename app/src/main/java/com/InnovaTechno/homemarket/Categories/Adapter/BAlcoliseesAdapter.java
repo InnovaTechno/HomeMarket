512d691af5ed68b1a8749d8dd740846bf6c0383b
ElizabethDum12 <elizabethdumervil@gmail.com>
2019-10-17 08:29:52 -0400

    Working on design

--- app/src/main/java/com/InnovaTechno/homemarket/Categories/Adapter/BAlcoliseesAdapter.java
+++ app/src/main/java/com/InnovaTechno/homemarket/Categories/Adapter/BAlcoliseesAdapter.java
@@ -1,99 +1,64 @@
 package com.InnovaTechno.homemarket.Categories.Adapter;
 
 import android.content.Context;
-import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.InnovaTechno.homemarket.Categories.Post.PostBAlcolisees;
-import com.InnovaTechno.homemarket.Items_Detail.ItemDetails;
+import com.InnovaTechno.homemarket.Categories.Post.row;
 import com.InnovaTechno.homemarket.R;
-import com.bumptech.glide.Glide;
-import com.parse.ParseFile;
 
 import java.util.List;
 
-public class BAlcoliseesAdapter  extends RecyclerView.Adapter<BAlcoliseesAdapter.ViewHolder> {
+public class BAlcoliseesAdapter extends RecyclerView.Adapter<BAlcoliseesAdapter.ViewHolder> {
 
-    private Context context;
-    private List<PostBAlcolisees> posts;
+    private Context mContext;
+    private List<row> mData;
 
-    public BAlcoliseesAdapter(Context context, List<PostBAlcolisees> posts) {
-        this.context = context;
-        this.posts = posts;
+    public BAlcoliseesAdapter (Context mContext, List<row> mData) {
+        this.mContext = mContext;
+        this.mData = mData;
     }
-
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view= LayoutInflater.from(context).inflate(R.layout.cardview_fruits_legumes, parent, false);
+        View view;
+        LayoutInflater mInflater = LayoutInflater.from(mContext);
+        view = mInflater.inflate(R.layout.row_item, parent, false);
         return new ViewHolder(view);
     }
 
-
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        PostBAlcolisees postBAlcolisees = posts.get(position);
-        holder.bind(postBAlcolisees);
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
+       // holder.tvName.setText(mData.get(position).getName());
+        holder.row_img.setImageResource(mData.get(position).getImg());
     }
 
     @Override
     public int getItemCount() {
-        return posts.size();
+        return mData.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
-        ImageView ivFruits_Legumes;
-        TextView tvName;
-        TextView tvDevise;
-        Button btnAddToCart;
-        CardView cvFruits_Legumes;
-        private TextView tvPrice;
+
+    class ViewHolder extends RecyclerView.ViewHolder  {
+        //TextView tvName;
+        ImageView row_img;
+
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-            ivFruits_Legumes = itemView.findViewById(R.id.ivFruits_Legumes);
-            tvName = itemView.findViewById(R.id.tvName);
-            tvDevise = itemView.findViewById(R.id.tvDevise);
-            btnAddToCart = itemView.findViewById(R.id.btnAddToCart);
-            cvFruits_Legumes = itemView.findViewById(R.id.cvFruits_Legumes);
-            tvPrice = itemView.findViewById(R.id.tvPrice);
+            mContext = itemView.getContext();
+            //tvName = (TextView) itemView.findViewById(R.id.tvName);
+            row_img = (ImageView) itemView.findViewById(R.id.row_img);
+
 
         }
-        public void bind(PostBAlcolisees post) {
-            ParseFile image = post.getImage();
-            if (image != null) {
-                Glide.with(context).load(image.getUrl()).into(ivFruits_Legumes);
-            }
-            tvName.setText(post.getName());
-            tvDevise.setText(post.getDevise());
-            tvPrice.setText(post.getPrice());
-            //tvPrice.setText(Post.getPrice());
-
-            //Passing the data to items details activity
-            cvFruits_Legumes.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    int position = getAdapterPosition();
-                    Intent intent = new Intent(context, ItemDetails.class);
-                    intent.putExtra("name", posts.get(position).getName());
-                    intent.putExtra("price", posts.get(position).getPrice());
-                    intent.putExtra("devise", posts.get(position).getDevise());
-                    intent.putExtra("productImage" , posts.get(position).getImage());
-                    context.startActivity(intent);
-
-                }
-            });
-        }
-
     }
 }
