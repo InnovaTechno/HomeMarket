4ddabcdf8232416033526b86e0b8a6a33d337ec1
ElizabethDum12 <elizabethdumervil@gmail.com>
2019-10-18 13:36:12 -0400

    'Livraison' and 'Pickup' added in the drawer

---
+++ app/src/main/java/com/InnovaTechno/homemarket/Livraison/Livraison.java
@@ -0,0 +1,16 @@
+package com.InnovaTechno.homemarket.Livraison;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.InnovaTechno.homemarket.R;
+
+public class Livraison extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_livraison);
+    }
+}
